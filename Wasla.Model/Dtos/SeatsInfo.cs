01dcfc295156c8540747937acb21c008b497c219
omar alaa <omaralaaelzanaty@gmail.com>
2023-12-18 09:35:32 +0200

    error

---
+++ Wasla.Model/Dtos/SeatsInfo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wasla.Model.Dtos
+{
+	public class ResevrationDto
+	{
+		public string CustomerId { get; set; }
+		public int TripId { get; set; } 
+		public List<SeatsInfo> Orders { get; set; }
+	}
+	public class SeatsInfo
+	{
+		public int SeatNum { get; set; }
+		public string QrCodeFile { get; set; }//Iformfile
+	}
+}
