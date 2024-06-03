58f9f93b989c4216ff50d2ef16212230f1e73e0a
Omar Alaa <94639386+Omar-Alaa-Elzanaty@users.noreply.github.com>
2024-06-03 18:38:48 +0300

    Fix Bug

--- Wasla.Services/EntitiesServices/PublicDriverServices/IDriverServices.cs
+++ Wasla.Services/EntitiesServices/PublicDriverServices/IDriverServices.cs
@@ -9,7 +9,7 @@ namespace Wasla.Services.EntitiesServices.PublicDriverServices
     public interface IDriverServices
     {
         Task<BaseResponse> GetPublicPackagesRequestAsync(string driverId);
-        Task<BaseResponse> ReviewPackagesRequest(int packageId, int status);
+        Task<BaseResponse> ReviewPackagesRequest(int packageId, PackageStatus status);
         Task<BaseResponse> GetDriverPublicPackagesAsync(string DriverId);
         Task<BaseResponse> GetProfileAsync(string userId);
         Task<BaseResponse> GetTripStatus(string userId);
