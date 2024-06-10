1405c8ce052a09f2c94378b5b0b431048ebb7839
Esraa Aseem <Esraaaseem68@gmail.com>
2024-06-10 04:56:27 +0300

    save changes in org

---
+++ Wasla.Model/Dtos/UpdateOrgProfileDto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wasla.Model.Dtos
+{
+    public class UpdateOrgProfileDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string MyProperty { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserName { get; set; }
+        public float MaxWeight { get; set; }
+        public float MinWeight { get; set; }
+        public string WebsiteLink { get; set; }
+        public IFormFile Logo { get; set; }
+
+      
+    }
+}
